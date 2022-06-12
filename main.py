db339a1b02bb2cbfb20d55dd61e78af8bc766a91
Aditya Singh <64327510+skylar69-wtf@users.noreply.github.com>
2022-06-12 23:17:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import json
 import requests
 import time
 import smtplib
-
+# enter crypto symbol:- example: bitcoin = btc
 f = input("Enter the Crypto Short Nam: \n")
+# Enter your price you want to get alerted
 price_target = int(input("Enter Price target: "))
+#Enter your price you want to get alerted for loss
 loss_target = int(input("Enter Loss stop: "))
 
 while True:
-    url = "https://api.nomics.com/v1/currencies/ticker?key=1e0c0c160bc402daf0da9293ca1f7e1c2c6a3022&ids="+str(f).upper()
+    #paste your api key with out qoatations
+    url = "https://api.nomics.com/v1/currencies/ticker?key="paste your key here"="+str(f).upper()
     response = requests.session()
     req = response.get(url).text
     load = json.loads(req)
@@ -19,17 +22,21 @@ while True:
     if price > price_target :
         s = smtplib.SMTP('smtp.gmail.com', 587)
         s.starttls()
-        s.login("skylarcryptoalert@gmail.com", "mmwzcribduqqxnea")
+        #enter your login details
+        s.login("sendermail", "senderpass")
         message = "its time to sell your coin"
-        s.sendmail("skylarcryptoalert@gmail.com", "nothuman1701@gmail.com", message)
+        # enter sendermail and recivermail
+        s.sendmail("sendermail", "recivermail", message)
         print("MAIL SENT")
         s.quit()
     elif price < loss_target :
         s = smtplib.SMTP('smtp.gmail.com', 587)
         s.starttls()
-        s.login("skylarcryptoalert@gmail.com", "mmwzcribduqqxnea")
+        #enter your login details
+        s.login("sendermail", "senderpass")
+        #edit messege as per your choice
         message = "its time to buy your coin"
-        s.sendmail("skylarcryptoalert@gmail.com", "nothuman1701@gmail.com", message)
+        s.sendmail("sendermail", "recivermail", message)
         print("MAIL SENT")
         s.quit()
 
